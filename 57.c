9e2e61cfe56d56625cbd6aaf48ae3a3bcb1de988
Joshy Johns <35687903+JOSHYJOHNS@users.noreply.github.com>
2018-02-21 14:28:35 +0530

    Create 57.c

---
+++ 57.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include<conio.h> 
+int main()
+{
+      int i, n;
+      printf("Enter total number of elements(1 to 100):");
+      scanf("%f", &arr[i]);
+      return 0;
+     }
