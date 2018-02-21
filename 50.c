b9cf73c025a3b7fd99c0b3215bfab9184aae64d6
Joshy Johns <35687903+JOSHYJOHNS@users.noreply.github.com>
2018-02-21 14:10:13 +0530

    Create 50.c

---
+++ 50.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i, n;
+    float arr[100];
+    printf("Enter total number of elements(1 to 100): ");
+       printf("Enter Number %d: ", i+1);
+       scanf("%f", &arr[i]);
+    for(i = 1; i < n; ++
+    printf("Largest element = %.2f", arr[0]);
+    return 0;
+}
